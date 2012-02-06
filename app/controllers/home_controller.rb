768062d3a2006fce92b03bcab65da9345320d07d
Bruno Coelho <bruno.coelho@bemind.com.br>
2012-02-06 17:57:13 -0300

    Permissao de escrita no mural e recuperacao de email.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ActionController::Base
   protect_from_forgery
   
-   def index   
+   def index
    	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
-		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream") 	
+    @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream, publish_stream, email") # leitura, escrita e email. 	
 		puts session.to_s + "<<< session"
 
   	respond_to do |format|
@@ -18,19 +18,20 @@ class HomeController < ActionController::Base
 		end		
 
 		 # auth established, now do a graph call:
-		  
 		@api = Koala::Facebook::API.new(session[:access_token])
+
+    # escrevendo no mural.
+    # @api.put_wall_post("Escrevendo no mural")
+    
 		begin
 			@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
 		rescue Exception=>ex
 			puts ex.message
 		end
-		
   
  		respond_to do |format|
 		 format.html {   }			 
 		end
-		
 	
 	end
 end
